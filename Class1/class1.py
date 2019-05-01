7578b771368b0558af20201ec1fc2377ba8ad0a7
Caroline Mascarin <42577807+carolinemascarin@users.noreply.github.com>
2019-05-01 18:41:27 +0100

    Homework from Session 1
    - Create a Python file and run it from your command line
    - Do basic math operations with integer and floating point numbers
    - Do basic string manipulation
    - Create and manipulate variables
    - Print numbers, strings, and formatted text
    - Use comments in your code

---
+++ Class1/class1.py
@@ -0,0 +1,41 @@
+"""
+First class of CF:G class, these are the first exercises we did in class
+
+"""
+
+#test 1
+
+print 'Hello, World!'
+print 5-6
+print 8*9
+print 5000/25
+
+#test 2
+
+print "Bob" * 3
+print "hello".upper()
+print "the lord of the rings".title()
+
+# test 3
+
+age = 5
+print age
+age = "almost three"
+print age
+a_longer_name = "hello, CFG"
+print a_longer_name
+
+#test 4
+
+age = 5
+like = "painting"
+
+age_description = "My age is {0} and I like {1}".format(age, like)
+print age_description
+
+#test 5
+
+print 10/3
+print 10.0/3.0
+print 10% 3
+print 10.0 % 3.0
---
+++ Class1/homework1.py
@@ -0,0 +1,25 @@
+#homework from session 1
+
+"""
+1. Create a new Python file with the following statements in it, then run it. Were the
+things that were printed what you expected to see?
+
+"""
+a = 1
+a = a + 1
+print a
+b = "hello"
+print b
+c = b.title()
+print b
+print c
+d = "hello"
+e = d.title()
+print d
+print e
+name = "Dave"
+f = "Hello {0}! ".format(name)
+print f
+name = "Sarah"
+print f
+print f *5
